86eb7ab1561d4c34bd92290c2a4f227556d83cb3
Kareem Saleeb <100xkoko@gmail.com>
2025-10-15 11:51:19 +0300

    SelectionSort.cpp

--- SelectionSort.cpp
+++ SelectionSort.cpp
@@ -1,11 +1,29 @@
-void selectionSort(int arr[], int n) {
-    for (int i = 0; i < n - 1; i++) {
+
+
+void selectionSort(int arr[], int size) {
+    for (int i = 0; i < size - 1; i++) {
         int min_idx = i;
-        for (int j = i + 1; j < n; j++) {
-            if (arr[j] < arr[min_idx]) {
+        for (int j = i + 1; j < size; j++)
+            if (arr[j] < arr[min_idx])
                 min_idx = j;
-            }
-        }
-        swap(arr[i], arr[min_idx]);
+        int temp = arr[min_idx];
+        arr[min_idx] = arr[i];
+        arr[i] = temp;
     }
+}
+void printArray(int arr[], int size) {
+    for (int i = 0; i < size; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+
+int main() {
+    int arr[] = {64, 34, 25, 12, 22, 11, 90};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    printf("Original array: ");
+    printArray(arr, size);
+    selectionSort(arr, size);
+    printf("Sorted array: ");
+    printArray(arr, size);
+    return 0;
 }
